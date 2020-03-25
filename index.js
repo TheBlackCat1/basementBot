aa93330c6cb3e4377106842b91e517da5d9e0b4d
Carter Hively <55219126+TheBlackCat1@users.noreply.github.com>
2020-03-25 15:28:14 -0700

    Added more debugging

--- index.js
+++ index.js
@@ -7,26 +7,27 @@ const PREFIX = '!!'
 
 bot.on('ready', () =>{
     console.log('This bot is online!');
+    console.log(bot.channels);
 })
 
-bot.on('message', msg=>{
-    let args = msg.content.substring(PREFIX.length).split(" ");
+// bot.on('message', msg=>{
+//     let args = msg.content.substring(PREFIX.length).split(" ");
 
-    switch(args[0]){
-        case 'ping':
-            msg.channel.send('pong');
-            break;
-        case 'rr':
-            if(args.length >= 2){
-                msg.react(args[1]);
-            }
-            break;
-        case 'm':
-            if(args.length >= 3){
-                var ch = args[1]
-                ch.send(args[2]);
-            }
-    }
-})
+//     switch(args[0]){
+//         case 'ping':
+//             msg.channel.send('pong');
+//             break;
+//         case 'rr':
+//             if(args.length >= 2){
+//                 msg.react(args[1]);
+//             }
+//             break;
+//         case 'm':
+//             if(args.length >= 3){
+//                 var ch = args[1]
+//                 ch.send(args[2]);
+//             }
+//     }
+// })
 
 bot.login(token);
