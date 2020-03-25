b63f9f705528d1c24a7c1da168311179eb5dd9c1
Carter Hively <55219126+TheBlackCat1@users.noreply.github.com>
2020-03-25 15:39:34 -0700

    added replying to m

--- index.js
+++ index.js
@@ -10,24 +10,27 @@ bot.on('ready', () =>{
     console.log(bot.channels);
 })
 
-// bot.on('message', msg=>{
-//     let args = msg.content.substring(PREFIX.length).split(" ");
+bot.on('message', msg=>{
+    let args = msg.content.substring(PREFIX.length).split(" ");
 
-//     switch(args[0]){
-//         case 'ping':
-//             msg.channel.send('pong');
-//             break;
-//         case 'rr':
-//             if(args.length >= 2){
-//                 msg.react(args[1]);
-//             }
-//             break;
-//         case 'm':
-//             if(args.length >= 3){
-//                 var ch = args[1]
-//                 ch.send(args[2]);
-//             }
-//     }
-// })
+    switch(args[0]){
+        case 'ping':
+            msg.channel.send('pong');
+            break;
+        case 'rr':
+            if(args.length >= 2){
+                msg.react(args[1]);
+            }
+            break;
+        case 'm':
+            if(args.length >= 3){
+                var i;
+                for(i in bot.channels){
+                    msg.channel.send(i);
+                }
+            }
+            break;
+    }
+})
 
 bot.login(token);
