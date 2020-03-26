e7399cbbb69f8640de3889917ffd54918501abc3
Carter Hively <55219126+TheBlackCat1@users.noreply.github.com>
2020-03-26 10:14:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PREFIX = '!!'
 
 bot.on('ready', () =>{
     console.log('This bot is online!');
-    console.log(bot.channels);
+    //console.log(bot.channels);
     bot.user.setActivity('OASIS', {type: 'PLAYING'});
 })
 
@@ -21,9 +21,18 @@ bot.on('message', msg=>{
         case 'rr':
             if(args.length >= 2){
                 msg.react(args[1]);
-                console.log(msg.reactions.cache);
+                console.log(msg.reactions.cache.get());
             }
             break;
+        case 'gm':
+            //!!gm channelID messageID
+            if(args.length >= 3){
+                //do stuff
+                for(i in bot.channels.cache){
+                    console.log(bot.channels.fetch(args[1]));
+                }
+                msg.delete();
+            }
     }
 })
 
